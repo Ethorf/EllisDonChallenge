b5f3e3b1b451b87c484b171a83fcfdb7e11f83fe
Luke Osgood <lukemosgood@gmail.com>
2020-01-14 12:21:35 -0500

    moved task into main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Task from '../Task/Task';
 
 export default class App extends() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  render() {
+    return (
+      <div className='main'>
+      <Task />
     </div>
-  );
+    );
+  }
 }
 
 
