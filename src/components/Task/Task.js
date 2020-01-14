f6b0899cb3de29fa754a75ec0c057fb4c9811bea
Luke Osgood <lukemosgood@gmail.com>
2020-01-14 11:57:13 -0500

    basic task component

---
+++ src/components/Task/Task.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Task.scss';
+
+class Task extends React.Component {
+	state = {
+		data: []
+	};
+
+	render() {
+		
+
+		return (
+			<div className='task'>
+        <h1 className='task__title'>Task</h1>
+        <p className='task__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptate vitae incidunt ex nemo hic laborum veniam rem eius, vel aperiam tempora est corporis repudiandae, distinctio atque saepe fugiat officia.</p>
+        <div className='task__buttons'>
+          <button className='task__button'>Accept</button>
+          <button className='task__button'>Decline</button>
+          <button className='task__button'>Details</button>
+        </div>
+      </div>
+		);
+	}
+}
+
+export default Task;
