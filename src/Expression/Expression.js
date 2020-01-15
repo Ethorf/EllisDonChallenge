ffd5a8aeeae1329577b9db06bdff0bd572565710
John Heckbert <johnheckbert@yahoo.ca>
2020-01-15 12:56:46 -0500

    added a dead end for our API calls

--- src/Expression/Expression.js
+++ src/Expression/Expression.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // import styling
 import './Expression.scss';
@@ -6,9 +7,38 @@ import './Expression.scss';
 export default class Expression extends Component {
 
     handleClick(e) {
-        console.log(e);
         e.preventDefault();
-    }
+        let conditionTest = e.target.name.value;
+        let conditionTrue = e.target.iftrue.value;
+        let conditionFalse = e.target.iffalse.value;
+        axios
+        .post("http://localhost:5500/expression", {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: {
+            condition: conditionTest,
+            ifTrue: conditionTrue,
+            ifFalse: conditionFalse
+          }
+        })
+        .then(result => {
+            let data = result.data
+            let clearCondition = document.querySelector('.expression__condition-field');
+            clearCondition.value = " ";
+            let clearTest1 = document.querySelector('.expression__true-field');
+            clearTest1.value = " ";
+            let clearTest2 = document.querySelector('.expression__false-field');
+            clearTest2.value = " ";
+            let resetResponse = document.querySelector('.expression__response-field');
+            resetResponse.value = data;
+;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      };
+    
 
     render() {
         return (
@@ -20,14 +50,20 @@ export default class Expression extends Component {
                         <input className="expression__condition-field" type="text" name="name" id="testCondition"></input>
                     </div>
                     <div className="expression__form-true">
-                        <label className="expression__true-label" htmlFor="true">What do you want to happen when true?</label>
-                        <input className="expression__true-field" type="text" name="true" id="testTrue"></input>
+                        <label className="expression__true-label" htmlFor="iftrue">What do you want to happen when true?</label>
+                        <input className="expression__true-field" type="text" name="iftrue" id="testTrue"></input>
                     </div>
                     <div className="expression__form-false">
-                        <label className="expression__false-label" htmlFor="false">What do you want to happen when false?</label>
-                        <input className="expression__false-field" type="text" name="false" id="testFalse"></input>
+                        <label className="expression__false-label" htmlFor="iffalse">What do you want to happen when false?</label>
+                        <input className="expression__false-field" type="text" name="iffalse" id="testFalse"></input>
                     </div>
                     <button className="expression__button">Submit</button>
+                </form>
+                <form className="expression__response" id="responseCapture">
+                    <div className="expression__response-area">
+                        <label className="expression__response-label" htmlFor="response">Response</label>
+                        <input className="expression__response-field" type="textarea" name="response" id="response" value="holding"></input>
+                    </div>
                 </form>
             </div>
         );
