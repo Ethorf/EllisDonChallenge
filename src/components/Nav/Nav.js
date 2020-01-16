5adaa65c1f518ea4daed3b0a21c5aace50297da2
Ivan Chau <ivan.chau1023@gmail.com>
2020-01-16 18:20:45 -0500

    styles

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Nav.scss'
 
 
 export default class Sidebar extends React.Component {
   render () {
     return (
-      <div >
-        <p>This is the Nav</p>
+      <div className="nav">
+        <p>EllisDon</p>
+        <p>WorkFlow</p>
       </div>
     );
   }
