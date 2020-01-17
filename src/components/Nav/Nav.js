94d627d9d75af3d6505b832be79efc68c2ce166c
John Heckbert <johnheckbert@yahoo.ca>
2020-01-17 10:06:38 -0500

    added ED logo to presentation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Nav.scss'
 import { Link } from 'react-router-dom';
 
+// import images
+import logo from './ellisdon t logo.png';
+
 
 export default class Sidebar extends React.Component {
   render () {
     return (
       <div className="nav">
-        <p>EllisDon</p>
+        <img src={logo} width="150" alt="Ellis Don Logo" />
         <Link to={'/'}><p>Expression / User Task</p></Link>
         <Link to={'/workflow'}><p>WorkFlow</p></Link>
       </div>
